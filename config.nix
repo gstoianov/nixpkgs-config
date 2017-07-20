8dcb55bea5399d0e0e199f92e80e944d7d489d4f
George Stoianov <gstoianov@gmail.com>
2017-07-20 13:12:07 +0300

    Move some packages from system environment
    - Add rstudio

--- config.nix
+++ config.nix
@@ -1,46 +1,79 @@
 {
   allowUnfree = true;
+  wine.build = "wine32";
 
   firefox = {
     enableAdobeFlash = true;
   };
-  
-  packageOverrides = pkgs: {
+
+  packageOverrides = super: let self = super.pkgs; in {
     # jdk = pkgs.oraclejdk8;
-    ruby = pkgs.ruby_1_9_3;
-    nodejs = pkgs.nodejs-6_x;
-    nodePackages = pkgs.nodePackages_6_x;
-    idea14_oracle = pkgs.idea.idea14-community.override {
-      jdk = pkgs.oraclejdk8;
+    # ruby = pkgs.ruby_1_9_3;
+    # nodejs = pkgs.nodejs-6_x;
+    # npm2nix = pkgs.nodePackages_4_x.npm2nix;
+    # nodePackages = pkgs.nodePackages_6_x;
+    # idea14_oracle = pkgs.idea.idea14-community.override {
+    #   jdk = pkgs.oraclejdk8;
+    # };
+
+    # idea15_ult_oracle = pkgs.idea.idea-ultimate.override {
+    #   jdk = pkgs.oraclejdk8;
+    # };
+
+    # rEnv = super.rWrapper.override {
+    #   packages = with self.rPackages; [
+    #     devtools
+    #     ggplot2
+    #     reshape2
+    #     yaml
+    #     optparse
+    #     data_table
+    #   ];
+    # };
+
+    rstudioEnv = super.rstudioWrapper.override {
+      packages = with self.rPackages; [
+        tidyverse pryr htmltools htmlwidgets
+      ];
+      # R = self.rEnv;
+      #   useRPackages = true;
     };
 
-    idea15_ult_oracle = pkgs.idea.idea-ultimate.override {
-      jdk = pkgs.oraclejdk8;
-    };
+    myPackages = with self; [
+      stack deluge skype slack vlc mc chromium sublime3
+      bind nix-repl glxinfo pciutils usbutils coreutils
+      playonlinux wine
+      git wget
+      calibre
+    ];
 
-    myemacs = pkgs.emacsWithPackages (with pkgs.emacsPackagesNg; [
-      magit haskell-mode
+    myemacs = super.emacsWithPackages (with self.emacsPackagesNg; [
+      magit
+
+      melpaPackages.haskell-mode
+      melpaPackages.intero
+
       # auctex
       melpaPackages.js2-mode
       melpaPackages.tern
-      
+
       melpaPackages.lua-mode
       melpaPackages.nix-mode
       melpaPackages.color-theme-solarized
 
       melpaPackages.dockerfile-mode
 
-      melpaPackages.helm-projectile
+      # melpaPackages.helm-projectile
       melpaPackages.ensime
       melpaPackages.scala-mode
       melpaPackages.jdee
 
-      melpaPackages.dirtree
+      # melpaPackages.dirtree
     ]);
 
-    ghc = pkgs.haskell.packages.ghc7103.ghcWithPackages (hpkgs : [
-      hpkgs.suffixtree
-    ]);
+    # ghc = pkgs.haskellPackages.ghcWithPackages (hpkgs : [
+    #   hpkgs.suffixtree
+    # ]);
 
   };
 
